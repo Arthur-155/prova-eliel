671003815cbcef92ab111d5c0f4f1a122b4bd4a1
arthur bergamo <arthur_bergamo@hotmail.com>
2025-10-23 22:22:31 -0300

    nao sei

--- src/main/java/aula/eliel/aula/repository/LibRepository.java
+++ src/main/java/aula/eliel/aula/repository/LibRepository.java
@@ -3,6 +3,8 @@ package aula.eliel.aula.repository;
 import aula.eliel.aula.model.Biblioteca;
 import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LibRepository extends JpaRepository<Biblioteca,Long> {
 }
